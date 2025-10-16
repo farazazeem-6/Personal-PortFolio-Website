3d7bb8041442500d02ead78b10107e863d9a1a36
farazazeem-6 <faraz.tacticminds.com>
2025-10-17 02:00:32 +0500

    add the theme button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React, { Fragment } from "react";
+import React, { createContext, Fragment, useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Hero from "./pages/Hero/Hero";
+import LocomotiveScroll from "locomotive-scroll";
+import About from "./pages/About/About";
+const locomotiveScroll = new LocomotiveScroll();
 
+const ThemeContext = createContext();
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }
+
+    useEffect(() => {
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(theme);
+  }, [theme]);
+
   return (
-    <Fragment>
-      <Header/>
-      <Hero/>
-    </Fragment>
+    <div className={theme}>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <Header />
+        <Hero />
+        <About />
+      </ThemeContext.Provider>
+    </div>
   );
 }
 
 export default App;
+export { ThemeContext };
