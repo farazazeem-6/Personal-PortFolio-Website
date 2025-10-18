f2f13a49a0952ef5b5830e4d798b97e1babbd652
farazazeem-6 <faraz.tacticminds.com>
2025-10-19 00:24:15 +0500

    add button

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,17 +3,20 @@ import styles from "../About/About.module.css";
 import aboutImg1 from "../../utils/images/about1.jpg";
 import aboutImg2 from "../../utils/images/about2.jpg";
 import aboutImg3 from "../../utils/images/about3.jpg";
-import {Button} from '@mui/material'
-import SendIcon from '@mui/icons-material/Send';
+import { Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 function About() {
   return (
     <div className={styles.aboutContainer}>
-      <h1 data-scroll data-scroll-direction="horizontal">About Me</h1>
+      <h1 data-scroll data-scroll-direction="horizontal">
+        About Me
+      </h1>
       <div className={styles.aboutData}>
         <div className={styles.aboutImg}>
           <img src={aboutImg1} alt="" />
         </div>
+
         <div className={styles.abouttext}>
           <p>
             <h4>My Introduction</h4>
@@ -23,11 +26,28 @@ function About() {
             Tailwind CSS, and a growing toolkit of UI libraries and animation
             frameworks
           </p>
-        </div>
-         <div className="resumeBtn">
-          <Button variant="contained" endIcon={<SendIcon/>}>
-            Download CV
-          </Button>
+          <div className={styles.resumeBtn}>
+            <Button
+            className={styles.resumeBtnStyle}
+              sx={{
+                backgroundColor: "transparent",
+                border: "1px solid #ccc",
+                boxShadow: "none",
+                padding:"10px 20px",
+                color:'rgb(95,85,85)',
+                fontFamily:' "Montserrat", sans-serif',
+                fontSize:'1.2rem',
+               borderRadius:'15px',
+               textTransform:'none',
+               fontWeight:'600',
+               letterSpacing:'2px'
+              }}
+              variant="contained"
+              endIcon={<SendIcon size={'large'} />}
+            >
+              Download CV
+            </Button>
+          </div>
         </div>
       </div>
     </div>
