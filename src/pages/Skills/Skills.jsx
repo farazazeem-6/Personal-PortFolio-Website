dbd19c81c1712fb52959a2171b2b423e6a8cb6ac
farazazeem-6 <faraz.tacticminds.com>
2025-10-19 23:04:38 +0500

    skills

---
+++ src/pages/Skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../../pages/Skills/Skills.module.css'
+
+function Skills() {
+  return (
+    <div>
+        <h1>Personal Skills</h1>
+        <div className={styles.skillsContainer}>
+        
+    </div> 
+    </div>
+   
+  )
+}
+
+export default Skills
